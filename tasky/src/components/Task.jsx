de781905428e20786540b92104bd59c56a4b958d
Dmitriy Nosatenko <dmitriynosa@gmail.com>
2026-09-10 07:46:08 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,14 @@
+const Task = (props) => {
+
+        return (
+        <div className="card">
+            <p className="title"> {props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p className="bold"> {props.description}</p>
+        </div>
+    )
+
+}
+
+export default Task;
+
