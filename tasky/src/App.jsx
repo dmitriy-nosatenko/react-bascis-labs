9c49307df9ad6870b3d6c97ab11a5256548eebf0
Dmitriy Nosatenko <dmitriynosa@gmail.com>
2026-09-16 10:39:49 +0100

    test

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
     const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title: "Dishes", description: "Empty dishwasher", deadline: "Today" }, 
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" }, 
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low" }, 
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" }, 
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "High" }
     ]
   });
 
@@ -19,9 +19,11 @@ function App() {
       
        {taskState.tasks.map((task) => (              
     <Task 
+      key={task.id} 
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority} 
     />
   ))} 
 
