1388007a8e9030e57581d5b743b703c8561fc5f4
Dmitriy Nosatenko <dmitriynosa@gmail.com>
2026-09-18 16:40:43 +0100

    Tasky Lab 2b - Exercise done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -47,6 +47,9 @@ function App() {
       case "deadline":
           form.deadline = event.target.value;
           break;
+      case "priority":
+        form.priority = event.target.value;
+        break; 
       default:
           form = formState;
     }
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,7 +1,13 @@
+const priorityColors = {
+    Low: '#5bb4c4',
+    Medium: '#f39c12',
+    High: '#e74c3c', 
+}; 
+
 const Task = (props) => {
 
         return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : priorityColors[props.priority]}}>
             <p className="title"> {props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="bold"> {props.description}</p>
