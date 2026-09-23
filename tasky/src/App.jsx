d9ccc99338c375c48b3d07c9fd6c38e7a146b0b4
Dmitriy Nosatenko <dmitriynosa@gmail.com>
2026-09-23 09:43:29 +0100

    Tasky Lab 3 done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,6 +3,9 @@ import Task from './components/Task';
 import React, { useState } from 'react'; 
 import AddTaskForm from './components/Form';
 import { v4 as uuidv4 } from 'uuid'; 
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container'; 
+import Grid from '@mui/material/Grid';
 
 function App() {
 
@@ -72,21 +75,61 @@ function App() {
   return (
 
     <div className="container">
-      <h1>Tasky</h1>
+      <Container component="main">
+        <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        gutterBottom
+        sx={{
+          backgrounColor: 'gray',
+          textAlign: 'center',
+          color: 'white',
+          padding: '20px',
+          margin: '20px 0 40px 0',
+          borderRadius: '4px'
+        }}
+        >
+          Tasky 
+        </Typography>
+        </Container>
       
-       {taskState.tasks.map((task, index) => (              
-    <Task 
-      key={task.id} 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      priority={task.priority}
-      markDone= {() => doneHandler(index)}
-      done={task.done}
-      deleteTask = {() => deleteHandler(index)} 
-    />
-  ))}
-<AddTaskForm submit={formSubmitHandler} change={formChangeHandler}/>
+<Container maxWidth="md" component="main">
+  <Grid
+    container
+    spacing={5}
+    alignItems="flex-start"
+    justifyContent="center"
+  >
+    {taskState.tasks.map((task, index) => (
+      <Task
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+        done={task.done}
+        key={task.id}
+        markDone={() => doneHandler(index)}
+        deleteTask={() => deleteHandler(index)}
+      />
+    ))}
+  </Grid>
+</Container>
+
+<Container
+  component="footer"
+  sx={{
+    borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+    my: 6,
+    py: 6, 
+  }}
+  >
+    <Grid container justifyContent="center">
+      <AddTaskForm 
+        submit={formSubmitHandler} 
+        change={formChangeHandler}
+  />
+  </Grid>
+</Container>
     </div>
   )
 }
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,3 +1,12 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
 const priorityColors = {
     Low: '#5bb4c4',
     Medium: '#f39c12',
@@ -7,14 +16,79 @@ const priorityColors = {
 const Task = (props) => {
 
         return (
-        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : priorityColors[props.priority]}}>
-            <p className="title"> {props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p className="bold"> {props.description}</p>
-            <p className="bold"> {props.priority}</p>
-            <button className="doneButton"onClick={props.markDone}>Done</button>
-            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
-        </div>
+     <Grid
+  key={props.id}
+  size={{ xs: 12, md: 4 }}
+>
+  <Card
+    sx={{
+      backgroundColor: props.done ? 'lightgrey' : 'lightblue',
+      padding: '20px'
+    }}
+  >
+    <CardHeader
+    title={props.title}
+    sx={{
+        backgroundColor: 'white',
+        borderRadius: '3px',
+        padding: '20px',
+        textAlign: 'center' 
+    }}
+    />
+
+    <CardContent>
+        <Box
+        sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'baseline',
+            mb: 2,
+            padding: '20px' 
+        }}
+        >
+            <Typography
+            component="p"
+            variant="subtitle2"
+            color="text.primary"
+            >
+                Due: {props.deadline}
+            </Typography>
+        </Box>
+        <Typography
+        component="p"
+        variant="subtitle1"
+        align="center"
+        sx={{ fontStyle: 'italic' }}
+        >
+            {props.description}
+        </Typography>
+    </CardContent>
+
+    <CardActions
+    sx={{
+        justifyContent: 'space-between',
+        padding: '20px'
+    }}
+    >
+        <Button
+        variant="contained"
+        size="small"
+        color="success"
+        onClick={props.markDone}
+        >
+            Done 
+        </Button>
+        <Button
+        variant="contained"
+        size="small"
+        color="error"
+        onClick={props.deleteTask}
+        >
+            Delete 
+        </Button>
+    </CardActions>
+  </Card>
+</Grid>
     )
 }
 
